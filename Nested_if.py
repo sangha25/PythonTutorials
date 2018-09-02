b28203f5e7dd79fb27831f1e208ab396de283b3d
sangha25 <sanghapal25@gmail.com>
2018-09-02 12:32:46 +0530

    improved code.

---
+++ Nested_if.py
@@ -0,0 +1,9 @@
+Age= input("enter your age")
+if(int(Age)>18):
+    print("You are Welcome")
+    if(int(Age)<30):
+        print("Your class is A")
+    else:
+        print("your class is B")
+else:
+    print("Sorry You aren't allowed to enroll")
