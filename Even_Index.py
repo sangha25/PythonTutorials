7a0cfa2225af1aa9367ac87c0fd092f7d06995e5
sangha25 <sanghapal25@gmail.com>
2018-10-25 16:11:31 +0530

    Update Even_Index.py

--- Even_Index.py
+++ Even_Index.py
@@ -1,1 +1,11 @@
+T = (1,2,3,4,5)
+print('Original Tuple is :',end=' ')
+print(T)
+l=[]
+for i in T:
+    if T.index(i)%2==1:
+        l.append(i)
+T2 = tuple(l)
+print('Values at Odd index are',end=' ')
+print(T2)
 
