e2fcc5bdd6a00521f5d5a17c01c5c1ac223ccb88
sangha25 <sanghapal25@gmail.com>
2018-10-25 16:13:41 +0530

    Update Even_Index.py

--- Even_Index.py
+++ Even_Index.py
@@ -1,8 +1,9 @@
 T = (1,2,3,4,5)
-print('Original Tuple is :',end=' ')
+print('Original Tuple elements are :',end=' ')
 print(T)
 l=[]
 for i in T:
+    #for elemnts having Remainder 0.
     if T.index(i)%2==0:
         l.append(i)
 T2 = tuple(l)
